a9c9a5f8d706f4376530169cd32499951f92d0e4
bennyzhvng <54347218+bennyzhvng@users.noreply.github.com>
2020-11-03 15:14:19 +1100

    Delete Billing.ps1

--- Powershell/Billing.ps1
+++
@@ -1,8 +0,0 @@
-# MSDN Subscription and Tenant
-Clear-AzContext -Scope CurrentUser -Force
-Connect-AzAccount
-Get-AzContext
-
-# For MSDN, "no invoice will be generated if there are no charges"
-$invoice = Get-AzBillingInvoice -Latest
-Invoke-WebRequest -Uri $invoice.DownloadUrl -OutFile ($home + '\' + $invoice.Name + '.pdf')
